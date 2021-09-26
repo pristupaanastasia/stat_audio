6ddef27b513a38c089d8a1028a98f8744831543c
pristupaanastasia <pristupa1998@mail.ru>
2021-09-26 14:00:25 +0300

    reverse bit

--- main.go
+++ main.go
@@ -199,10 +199,21 @@ func main() {
 			time.Sleep(time.Second)
 
 			packet.packetId = 3
+			packet.command = 3
+			packet.lenData = 4
+			data = SetByte(packet)
+			bytedata := SetData(0)
+			data = append(data, bytedata...)
+			n, err = s.Write(data)
+			if err != nil {
+				log.Fatal(err)
+			}
+			time.Sleep(time.Second)
+			packet.packetId = 4
 			packet.command = 0x0208
 
-			packet.lenData = 2
-			bytedata := SetData(48)
+			packet.lenData = 4
+			bytedata = SetData(48)
 			data = SetByte(packet)
 
 			data = append(data, bytedata...)
