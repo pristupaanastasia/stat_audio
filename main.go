93c13f7eb185ac8c466452d785522b5a1a4e1cec
pristupaanastasia <pristupa1998@mail.ru>
2021-09-27 22:26:10 +0300

    reverse bit

--- main.go
+++ main.go
@@ -151,7 +151,7 @@ func main() {
 			if err != nil {
 				log.Fatal(err)
 			}
-
+			s.Flush()
 			time.Sleep(time.Second / 2)
 			n, err := s.Write(data)
 			if err != nil {
@@ -186,7 +186,7 @@ func main() {
 			packet.packetId = 2
 			packet.command = 1
 			data = SetByte(packet)
-			s.Flush()
+
 			n, err = s.Write(data)
 			if err != nil {
 				log.Fatal(err)
